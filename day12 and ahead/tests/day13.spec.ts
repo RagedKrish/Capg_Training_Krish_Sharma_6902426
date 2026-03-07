3f75694ed4d3accf7b71d127332c871d20aa14db
RagedKrish <krish18sharma21@gmail.com>
2026-03-07 12:57:15 +0530

    Adding day 13 files

---
+++ day12 and ahead/tests/day13.spec.ts
@@ -0,0 +1,31 @@
+import {test} from "@playwright/test"
+
+ test("union",async ({page})=>{
+    await page.goto("https://www.amazon.com/");
+    await page.locator('//input[@id="twotabsearchtextbox"]').fill("phones");
+    await page.keyboard.press("Enter");
+    const items=await page.locator('(//a[@class="a-link-normal s-line-clamp-2 puis-line-clamp-3-for-col-4-and-8 s-link-style a-text-normal"]/descendant::span)[3] | (//a[@class="a-link-normal s-no-hover s-underline-text s-underline-link-text s-link-style a-text-normal"]/span[@class="a-price"]/span[@class="a-offscreen"])[1]')
+    console.log(await items.allTextContents());
+ })
+
+// getby methods
+// getbyText()
+// getbyAltText()
+// getbyLabel()
+// getbyTitle()
+// getbyPlaceholder()
+// getbyRole()
+// getbyTextId()
+
+test("Get_by_methods",async ({page})=>{
+    // await page.goto('https://practicetestautomation.com/practice-test-login/');
+    // await page.getByRole('textbox',{name:"username"}).fill('student')
+    // await page.getByRole('textbox',{name:"password"}).fill('Password123');
+    // await page.getByRole('button',{name:'Submit'}).click();
+    //await page.getByLabel('Username',{exact:true}).fill('student');
+    //await page.getByLabel('Password',{exact:true}).fill('Password123');
+    //await page.getByText("Submit").first().click();
+
+    await page.goto('https://sahil.com/');
+    await page.getByAltText('BLACK AND PINK LEHENGA SAREE').click();
+})
